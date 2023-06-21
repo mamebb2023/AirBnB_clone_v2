e20dffd89fdf8cbdc4f2f101b501210482ba18c1
Mohammednur <mamebb2023@gmail.com>
2023-06-21 19:18:13 +0300

    Setup mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,3 +5,4 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTFIED BY 'hbnb_dev_pwd';
 -- Grant permissions for the created user
 GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
